52dd3d48103578e869ac82aa2ddefb33f708fe57
Carl Ngovi <carl.ngovi@student.moringaschool.com>
2024-04-12 09:48:54 +0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+//CHECKOUT SECTION
 function addToCart(item) {
     // Create a new list item
-    let listItem = document.createElement("li");
+    let listItem = document.createElement("h3");
     listItem.textContent = item;
 
     // Add the list item to the cart
     document.getElementById("cart-items").appendChild(listItem);
 }
+// Function to open the popup
+function openPopup() {
+    document.getElementById("myPopup").style.display = "block";
+  }
+  
+  // Function to close the popup
+  function closePopup() {
+    document.getElementById("myPopup").style.display = "none";
+  }
+   
